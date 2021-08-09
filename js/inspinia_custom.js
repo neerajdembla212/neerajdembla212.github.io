e83c71ff1a12ab40dc1148bad6f2ce24c29a26a5
neerajdembla212 <dembla.neeraj212@gmail.com>
2021-08-09 08:13:58 +0530

    date utility functions and adding accept and reject actions

--- js/inspinia_custom.js
+++ js/inspinia_custom.js
@@ -86,3 +86,41 @@ function getCountryFlags(country) {
     default: return '';
   }
 }
+
+// utility method to format date eg output : 1 Feb 2021
+function formatDate(date) {
+  if (!isDateValid(date)) {
+    return '';
+  }
+  let ye = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+  let mo = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
+  let da = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+  return `${da} ${mo} ${ye}`;
+}
+
+// utility function to check valid date object
+function isDateValid(d) {
+  return d instanceof Date && !isNaN(d)
+}
+// utility method to calculate date difference between two dates eg output : x Years y Months z days
+function calculateDateDiff(a, b) {
+  if (!isDateValid(a) || !isDateValid(b)) {
+    return ''
+  }
+  const _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  // Discard the time and time-zone information.
+  const utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
+  const utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+  const days = Math.floor((utc2 - utc1) / _MS_PER_DAY);
+  const months = Math.floor(days / 31);
+  const years = Math.floor(months / 12);
+  if (years > 0) {
+    return `${years} Years`;
+  }
+  if (months > 0) {
+    return `${months} Months`;
+  }
+  if (days <= 31) {
+    return `${days} Days`
+  }
+}
