9d777362ad99d227dcaa767f1ca83731036779d3
neerajdembla212 <dembla.neeraj212@gmail.com>
2021-08-23 19:14:56 +0530

    login page

--- js/inspinia_custom.js
+++ js/inspinia_custom.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
+  if (!checkUserLogin()) {
+    window.location.href = window.origin + '/login.html'
+  }
   registerEventHandlers();
 });
+function checkUserLogin() {
+  return getCookie('accessToken')
+}
+
+function getCookie(name) {
+  var dc = document.cookie;
+  var prefix = name + "=";
+  var begin = dc.indexOf("; " + prefix);
+  if (begin == -1) {
+    begin = dc.indexOf(prefix);
+    if (begin != 0) return null;
+  } else {
+    begin += 2;
+    var end = document.cookie.indexOf(";", begin);
+    if (end == -1) {
+      end = dc.length;
+    }
+  }
+  // because unescape has been deprecated, replaced with decodeURI
+  return decodeURI(dc.substring(begin + prefix.length, end));
+}
 
 function registerEventHandlers() {
   // dropdown in navigation panel to switch accounts
