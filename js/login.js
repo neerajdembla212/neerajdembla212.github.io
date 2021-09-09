faccdddfea95387d85b4e06f075499dfbf0f3f70
neerajdembla212 <dembla.neeraj212@gmail.com>
2021-09-09 09:15:45 +0530

    register page desktop

--- js/login.js
+++ js/login.js
@@ -5,5 +5,8 @@
             document.cookie = "accessToken=12345678"
             window.location.href = window.location.origin + "/my-portfolio.html";
         })
+        $('.select2_dropdown').select2({
+            theme: 'bootstrap4',
+        });
     })
 })();
