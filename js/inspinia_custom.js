1266f698895342fbd4cd25729f94c5e43c2fec12
neerajdembla212 <dembla.neeraj212@gmail.com>
2021-09-12 18:39:23 +0530

    add timezone on EC

--- js/inspinia_custom.js
+++ js/inspinia_custom.js
@@ -305,7 +305,29 @@ function calculateDateDiff(a, b) {
     return `${days} Days`
   }
 }
+// utility to add timezone
+function getTimezoneOffset() {
+  var timezone_offset_min = new Date().getTimezoneOffset(),
+    offset_hrs = parseInt(Math.abs(timezone_offset_min / 60)),
+    offset_min = Math.abs(timezone_offset_min % 60),
+    timezone_standard;
 
+  if (offset_hrs < 10)
+    offset_hrs = '0' + offset_hrs;
+
+  if (offset_min < 10)
+    offset_min = '0' + offset_min;
+
+  // Add an opposite sign to the offset
+  // If offset is 0, it means timezone is UTC
+  if (timezone_offset_min < 0)
+    timezone_standard = '+' + offset_hrs + ':' + offset_min;
+  else if (timezone_offset_min > 0)
+    timezone_standard = '-' + offset_hrs + ':' + offset_min;
+  else if (timezone_offset_min == 0)
+    timezone_standard = 'Z';
+  return timezone_standard;
+}
 // activate tooltips globally (to be called by respective file after content is loaded)
 function activateTooltips() {
   $("[data-toggle='tooltip']").tooltip()
